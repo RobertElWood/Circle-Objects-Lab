5a6de9b9fa001a9b0032c2839548301a5d24b6df
Robert Woods <robert.woods@fulbrightmail.org>
2022-10-11 08:44:06 -0400

    initial commit

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects_Lab
+{
+    public class Circle
+    {
+        double Radius { get; set; }
+
+        public Circle (double radius)
+        {
+            Radius = radius;
+        }
+
+        public string CalculateCircumference () 
+        {
+            double circumference = (2 * Math.PI * Radius);
+            return FormatNumber(circumference);
+        }
+
+        public string CalculateArea () 
+        { 
+            double area = Math.PI * (Radius * Radius);
+            return FormatNumber(area);
+        }
+
+        private string FormatNumber(double result) 
+        { 
+            string resultFormat = Math.Round(result, 2).ToString();
+            return resultFormat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Circle_Objects_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tryAgain = true;
+
+            int CircleCounter = 0;
+
+            Console.WriteLine("Hello! Welcome to the Circle Calculator!\n");
+
+            while (tryAgain)
+            {
+                Console.WriteLine("Please enter the radius of the circle you'd like to calculate!\n");
+
+                try
+                {
+                    double userInput = double.Parse(Console.ReadLine());
+                    
+                    if (userInput < 0) 
+                    {
+                        Console.WriteLine("\nI'm sorry, please enter a positive number.\n");
+                        continue;
+                    }
+
+                    Circle input = new Circle(userInput);
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Your circumference was: " + input.CalculateCircumference() + "\n");
+                    Console.WriteLine("Your area was: " + input.CalculateArea() + "\n");
+                } 
+                catch (FormatException e)
+                {
+                    Console.WriteLine("\nI'm sorry, you didn't enter valid data. Please enter a number.\n");
+                    continue;
+                }
+
+                CircleCounter++;
+
+                tryAgain = RunAgain(CircleCounter);
+        
+            }
+
+        }
+
+        public static bool RunAgain(int CircleCounter)
+        {
+            Console.WriteLine("\nWould you like to create another circle? Y/N?\n");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "y")
+            {
+                Console.WriteLine();
+                return true;
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("\nGoodbye! You created " + CircleCounter + " Circle object(s).");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("\nI didn't understand that. Please try again!");
+                return RunAgain(CircleCounter);
+            }
+        }
+    }
+}
+
